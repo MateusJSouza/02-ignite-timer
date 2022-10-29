793753029a3c80938e899cdad2828939ccd5babf
Mateus Jesus <mateusjdev@gmail.com>
2022-10-28 22:58:43 -0300

    feat: header e layout da aplicação

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom'
-import { Header } from '../components/Header'
+import { Header } from '../../components/Header'
+import { LayoutContainer } from './styles'
 
 export function DefaultLayout() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       {/* Onde vai ocorrer a transição das páginas */}
       <Outlet />
-    </div>
+    </LayoutContainer>
   )
 }
