7cfece326caa63222a9fe94890e8c4bce43ea62c
Mateus Jesus <mateusjdev@gmail.com>
2022-10-29 13:52:41 -0300

    feat: componente de status

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -43,7 +43,7 @@ export const HistoryList = styled.div`
 
     td {
       background: ${(props) => props.theme['gray-700']};
-      border-top: ${(props) => props.theme['gray-800']};
+      border-top: 4px solid ${(props) => props.theme['gray-800']};
       padding: 1rem;
       font-size: 0.875rem;
       line-height: 1.6;
@@ -57,5 +57,30 @@ export const HistoryList = styled.div`
         padding-right: 1.5rem;
       }
     }
+  }
+`
+
+const STATUS_COLORS = {
+  yellow: 'yellow-500',
+  green: 'green-500',
+  red: 'red-500',
+} as const // para dizer ao typescript que esses valores sempre serão eles mesmos
+
+interface StatusProps {
+  // as interfaces são as chaves do meu tipo STATUS_COLORS
+  statusColor: keyof typeof STATUS_COLORS
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 999px;
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
   }
 `
