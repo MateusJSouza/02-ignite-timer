f35ba7d5a218c0ba63e4445eb7387e2638cf1ab3
Mateus Jesus <mateusjdev@gmail.com>
2022-11-01 11:22:43 -0300

    contexto entre rotas

---
+++ src/contexts/CyclesContext.tsx
@@ -0,0 +1,127 @@
+import { createContext, ReactNode, useState } from 'react'
+
+interface CreateCycleData {
+  task: string
+  minutesAmount: number
+}
+
+// Definição do formato dos ciclos
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+  startDate: Date
+  interruptedDate?: Date
+  finishedDate?: Date
+}
+
+interface CyclesContextType {
+  cycles: Cycle[]
+  activeCycle: Cycle | undefined
+  activeCycleId: string | null
+  amountSecondsPassed: number
+  setSecondsPassed: (seconds: number) => void
+  markCurrentCycleAsFinished: () => void
+  createNewCycle: (data: CreateCycleData) => void
+  interruptCurrentCycle: () => void
+}
+
+// {} as CyclesContextType -> para tipar o contexto
+export const CyclesContext = createContext({} as CyclesContextType)
+
+// ReactNode -> qualquer HTML ou JSX válido
+interface CyclesContextProviderProps {
+  children: ReactNode
+}
+
+// Componente responsável por
+export function CyclesContextProvider({
+  children,
+}: CyclesContextProviderProps) {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+
+  // o valor do id ativo do ciclo pode ser uma string ou nulo
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
+  // O tanto de segundos que já se passaram desde quando o ciclo está ativo
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
+  /* 
+    Percorrer o vetor de ciclos e encontrar um ciclo em que o ID do ciclo seja
+    igual ao ID do ciclo ativo
+  */
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  function setSecondsPassed(seconds: number) {
+    setAmountSecondsPassed(seconds)
+  }
+
+  function markCurrentCycleAsFinished() {
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return {
+            ...cycle,
+            finishedDate: new Date(),
+          }
+        } else return cycle
+      }),
+    )
+  }
+
+  function createNewCycle(data: CreateCycleData) {
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
+    }
+
+    /*
+      Adicionndo uma informação no array, copiando todos os ciclos que eu já
+      tenho e adicionar o novo ciclo no final.
+
+      Sempre que uma alteração de estado depender do valor anterior, devemos
+      usar o formato de arrow function
+    */
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
+    setAmountSecondsPassed(0)
+
+    // reset() // limpa os campos pro valor original, ou seja, os valores de defaultValues
+  }
+
+  function interruptCurrentCycle() {
+    setCycles((state) =>
+      // Map -> percorre cada ciclo e retorna cada um dos ciclos alterados ou não
+      state.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return {
+            ...cycle,
+            interruptedDate: new Date(),
+          }
+        } else return cycle
+      }),
+    )
+    setActiveCycleId(null)
+  }
+
+  return (
+    <CyclesContext.Provider
+      value={{
+        cycles,
+        activeCycle,
+        activeCycleId,
+        markCurrentCycleAsFinished,
+        amountSecondsPassed,
+        setSecondsPassed,
+        createNewCycle,
+        interruptCurrentCycle,
+      }}
+    >
+      {children}
+    </CyclesContext.Provider>
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,4 @@
 import { HandPalm, Play } from 'phosphor-react'
-import { createContext, useState } from 'react'
 
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
@@ -12,35 +11,12 @@ import {
 import { NewCycleForm } from './components/NewCycleForm'
 import { Countdown } from './components/Countdown'
 import { FormProvider, useForm } from 'react-hook-form'
-
-// Definição do formato dos ciclos
-interface Cycle {
-  id: string
-  task: string
-  minutesAmount: number
-  startDate: Date
-  interruptedDate?: Date
-  finishedDate?: Date
-}
-
-interface CyclesContextType {
-  activeCycle: Cycle | undefined
-  activeCycleId: string | null
-  amountSecondsPassed: number
-  setSecondsPassed: (seconds: number) => void
-  markCurrentCycleAsFinished: () => void
-}
-
-// {} as CyclesContextType -> para tipar o contexto
-export const CyclesContext = createContext({} as CyclesContextType)
+import { useContext } from 'react'
+import { CyclesContext } from '../../contexts/CyclesContext'
 
 export function Home() {
-  const [cycles, setCycles] = useState<Cycle[]>([])
-  // o valor do id ativo do ciclo pode ser uma string ou nulo
-  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
-
-  // O tanto de segundos que já se passaram desde quando o ciclo está ativo
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+  const { activeCycle, createNewCycle, interruptCurrentCycle } =
+    useContext(CyclesContext)
 
   const newCycleFormValidationSchema = zod.object({
     task: zod.string().min(1, 'Informe a tarefa'),
@@ -69,69 +45,7 @@ export function Home() {
     },
   })
 
-  const { handleSubmit, watch, reset } = newCycleForm
-
-  /* 
-    Percorrer o vetor de ciclos e encontrar um ciclo em que o ID do ciclo seja
-    igual ao ID do ciclo ativo
-  */
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
-
-  function setSecondsPassed(seconds: number) {
-    setAmountSecondsPassed(seconds)
-  }
-
-  function markCurrentCycleAsFinished() {
-    setCycles((state) =>
-      state.map((cycle) => {
-        if (cycle.id === activeCycleId) {
-          return {
-            ...cycle,
-            finishedDate: new Date(),
-          }
-        } else return cycle
-      }),
-    )
-  }
-
-  function handleCreateNewCycle(data: NewCycleFormData) {
-    const id = String(new Date().getTime())
-
-    const newCycle: Cycle = {
-      id,
-      task: data.task,
-      minutesAmount: data.minutesAmount,
-      startDate: new Date(),
-    }
-
-    /*
-      Adicionndo uma informação no array, copiando todos os ciclos que eu já
-      tenho e adicionar o novo ciclo no final.
-
-      Sempre que uma alteração de estado depender do valor anterior, devemos
-      usar o formato de arrow function
-    */
-    setCycles((state) => [...state, newCycle])
-    setActiveCycleId(id)
-    setAmountSecondsPassed(0)
-
-    reset() // limpa os campos pro valor original, ou seja, os valores de defaultValues
-  }
-
-  function handleInterruptCycle() {
-    setCycles((state) =>
-      // Map -> percorre cada ciclo e retorna cada um dos ciclos alterados ou não
-      state.map((cycle) => {
-        if (cycle.id === activeCycleId) {
-          return {
-            ...cycle,
-            interruptedDate: new Date(),
-          }
-        } else return cycle
-      }),
-    )
-    setActiveCycleId(null)
-  }
+  const { handleSubmit, watch /* reset */ } = newCycleForm
 
   // Observar os campos do formulário com o método watch
   const task = watch('task')
@@ -145,25 +59,15 @@ export function Home() {
 
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
-        <CyclesContext.Provider
-          value={{
-            activeCycle,
-            activeCycleId,
-            markCurrentCycleAsFinished,
-            amountSecondsPassed,
-            setSecondsPassed,
-          }}
-        >
-          {/* Herda as propripedades do newCycleForm para o FormProvider */}
-          <FormProvider {...newCycleForm}>
-            <NewCycleForm />
-          </FormProvider>
-          <Countdown />
-        </CyclesContext.Provider>
+      <form onSubmit={handleSubmit(createNewCycle)}>
+        {/* Herda as propripedades do newCycleForm para o FormProvider */}
+        <FormProvider {...newCycleForm}>
+          <NewCycleForm />
+        </FormProvider>
+        <Countdown />
 
         {activeCycle ? (
-          <StopCountdownButton onClick={handleInterruptCycle} type="button">
+          <StopCountdownButton onClick={interruptCurrentCycle} type="button">
             <HandPalm size={24} />
             Interromper
           </StopCountdownButton>
